8b88d0bdf938d2fdd21e2ff26500b5d3eeca1f95
Kimberly Vitug <kim_vitug05@yahoo.com>
2020-09-17 17:09:14 -0700

    add solution for total_medals_per_team method

--- lib/csv_practice.rb
+++ lib/csv_practice.rb
@@ -12,7 +12,9 @@ def get_all_olympic_athletes(filename)
 end
 
 def total_medals_per_team(olympic_data)
-
+  return olympic_data.filter { |athlete| athlete['Medal'] != 'NA' }
+             .group_by { |athlete| athlete['Team'] }
+             .to_h { |team, medals| [team, medals.size] }
 end
 
 def get_all_gold_medalists(olympic_data)
